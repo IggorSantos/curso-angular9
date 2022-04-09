6d8ecfa537a81787fd35e150be9729a00eb86214
Igor Santos <igor.sagitarius@gmail.com>
2022-04-09 11:33:18 -0300

    Criação da tabela de produtos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,12 @@ import { HttpClientModule } from '@angular/common/http'
 
 import { FormsModule} from '@angular/forms'
 import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule} from '@angular/material/input'
+import { MatInputModule} from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort'
 
 @NgModule({
   declarations: [
@@ -31,6 +36,8 @@ import { MatInputModule} from '@angular/material/input'
     HomeComponent,
     ProductCrudComponent,
     ProductCreateComponent,
+    ProductReadComponent,
+    ProductRead2Component,
   ],
   imports: [
     BrowserModule,
@@ -45,7 +52,10 @@ import { MatInputModule} from '@angular/material/input'
     HttpClientModule,
     FormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
