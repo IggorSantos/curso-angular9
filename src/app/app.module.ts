85e3170f4ce0905b8bb09bf75d07b03138255eda
Igor Santos <igor.sagitarius@gmail.com>
2022-04-18 16:40:58 -0300

    Criação da atualização do produto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,8 @@ import { ProductReadComponent } from './components/product/product-read/product-
 import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort'
+import { MatSortModule } from '@angular/material/sort';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component'
 
 @NgModule({
   declarations: [
@@ -38,6 +39,7 @@ import { MatSortModule } from '@angular/material/sort'
     ProductCreateComponent,
     ProductReadComponent,
     ProductRead2Component,
+    ProductUpdateComponent,
   ],
   imports: [
     BrowserModule,
